10d8688df408524d5796d695db152e8e674d4d95
AndyW22 <andrew.waller24@outlook.com>
2021-02-18 13:05:43 +0000

    Added addToDo function with GraphQL API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { GraphQLResult } from '@aws-amplify/api';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+import './styles.css';
+import awsExports from './aws-exports';
+import { ListTodosQuery } from './API';
+Amplify.configure(awsExports);
 
-function App() {
+const initialState = { name: '', description: '' };
+
+const App: React.FC = () => {
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState<any[]>([]);
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  function setInput(key: string, value: string) {
+    setFormState({ ...formState, [key]: value });
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = (await API.graphql(
+        graphqlOperation(listTodos),
+      )) as GraphQLResult<ListTodosQuery>;
+      const todos = todoData?.data?.listTodos?.items;
+      if (todos) {
+        setTodos(todos);
+      }
+    } catch (err) {
+      console.log('error fetching todos');
+    }
+  }
+
+  async function addTodo() {
+    try {
+      if (!formState.name || !formState.description) return;
+      const todo = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+    } catch (err) {
+      console.log('error creating todo:', err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="title">ToDo list</h2>
+      <input
+        onChange={(event) => setInput('name', event.target.value)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <input
+        onChange={(event) => setInput('description', event.target.value)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <button className="button" onClick={addTodo}>
+        Create Todo
+      </button>
+      {todos.map((todo, index) => (
+        <div className="todo" key={todo.id ? todo.id : index}>
+          <p className="todoName">{todo.name}</p>
+          <p className="toDoDescription">{todo.description}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    width: 400,
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  todo: { marginBottom: 15 },
+  input: {
+    border: 'none',
+    backgroundColor: '#ddd',
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: {
+    backgroundColor: 'black',
+    color: 'white',
+    outline: 'none',
+    fontSize: 18,
+    padding: '12px 0px',
+  },
+};
 
 export default App;
