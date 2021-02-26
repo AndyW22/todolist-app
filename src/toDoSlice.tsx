b42013dcd179d9e68dbc5603bce6e345aecd3a30
AndyW22 <andrew.waller24@outlook.com>
2021-02-26 12:13:29 +0000

    Added AsyncThunk for fetchToDos

--- src/toDoSlice.tsx
+++ src/toDoSlice.tsx
@@ -1,10 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { AppDispatch, RootState } from './redux/store';
 import { API, graphqlOperation } from 'aws-amplify';
 import { GraphQLResult } from '@aws-amplify/api';
 import { CreateTodoMutation, ListTodosQuery, Todo } from './API';
 import { listTodos } from './graphql/queries';
 import { createTodo } from './graphql/mutations';
+
+export const fetchToDos = createAsyncThunk<GraphQLResult<ListTodosQuery>>(
+  'todos/fetch',
+  async (ignore, thunkApi) => {
+    try {
+      const todoData = (await API.graphql(
+        graphqlOperation(listTodos),
+      )) as GraphQLResult<ListTodosQuery>;
+      return todoData;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  },
+);
 
 export const toDoSlice = createSlice({
   name: 'todos',
@@ -13,45 +27,18 @@ export const toDoSlice = createSlice({
     addToDo: (state, action) => {
       state.push({ ...action.payload });
     },
-    fetchToDosStart: (state) => {
-      return state;
-    },
-    fetchToDosSuccess: (state, action) => {
-      return action.payload;
-    },
-    fetchToDosFailure: (state, action) => {
-      console.log(`error fetching todos: ${action} `);
-      return state;
-    },
     removeToDo: (state, action) => {
       state.splice(action.payload, 1);
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchToDos.fulfilled, (state, action: any) => {
+      return action.payload.data?.listTodos?.items;
+    });
+  },
 });
 
-export const {
-  addToDo,
-  fetchToDosStart,
-  fetchToDosSuccess,
-  fetchToDosFailure,
-  removeToDo,
-} = toDoSlice.actions;
-
-export const fetchToDos = () => (dispatch: AppDispatch): void => {
-  async function fetch() {
-    try {
-      dispatch(fetchToDosStart());
-      const todoData = (await API.graphql(
-        graphqlOperation(listTodos),
-      )) as GraphQLResult<ListTodosQuery>;
-      const todos = todoData?.data?.listTodos?.items;
-      dispatch(fetchToDosSuccess(todos));
-    } catch (err) {
-      dispatch(fetchToDosFailure(err));
-    }
-  }
-  fetch();
-};
+export const { addToDo, removeToDo } = toDoSlice.actions;
 
 export const addToDoThunk = (name: string, description: string) => (
   dispatch: AppDispatch,
