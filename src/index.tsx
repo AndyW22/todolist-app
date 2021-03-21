7b9182a70174bea823c535af4b1b5068295826a9
AndyW22 <andrew.waller24@outlook.com>
2021-03-21 12:01:02 +0000

    Added react-snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import Amplify from 'aws-amplify';
 import awsExports from './aws-exports';
 import { Provider } from 'react-redux';
-import store, {persistor} from './redux/store';
+import store, { persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Spinner } from './styles/Styles';
+import { hydrate, render } from 'react-dom';
 Amplify.configure(awsExports);
 
-ReactDOM.render(
+const AppComponents = () => (
   <Provider store={store}>
     <PersistGate loading={<Spinner />} persistor={persistor}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </PersistGate>
-  </Provider>,
-  document.getElementById('root'),
+  </Provider>
 );
+
+const rootElement = document.getElementById('root');
+if (rootElement?.hasChildNodes()) {
+  hydrate(<AppComponents />, rootElement);
+} else {
+  render(<AppComponents />, rootElement);
+}
